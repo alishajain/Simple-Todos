d47edfa79114146e2ed68ece9c77bfc228609355
alishajain <jain.alisha14@gmail.com>
2017-02-25 17:13:01 +0530

    Hide Completed Task chekbox enabled

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,13 +1,27 @@
 import { Template } from 'meteor/templating';
+import { ReactiveDict } from 'meteor/reactive-dict';
 
 import { Tasks } from '../api/tasks.js';
 
 import './task.js';
 import './body.html';
 
+Template.body.onCreated(function bodyOnCreated() {
+    this.state = new ReactiveDict();
+});
+
 Template.body.helpers({
     tasks() {
-        return Tasks.find({}, { sort: { createdAt: -1 } });
+        const instance = Template.instance();
+        if (instance.state.get('hideCompleted')) {
+            //if hide completed is checked, filter tasks
+            return Tasks.find({ checked: { $ne: true } }, { sort: { createdAt: -1 } });
+        }
+            //Otherwise, return all of the tasks
+            return Tasks.find({}, { sort: { createdAt: -1 } });
+    },
+    incompleteCount() {
+        return tasks.find({ checked: { $ne: true } }) .count();
     },
 });
 
@@ -28,5 +42,9 @@ Template.body.events({
 
         //Clear form
         target.text.value = '';
+    },
+
+    'change .hide-completed input' (event, instance) {
+        instance.state.set('hidecompleted', event.target.checked);
     },
 });
